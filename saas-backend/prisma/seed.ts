551ef28122e362e22c9c19afbc99cdfd4b155d95
KEVIN01306 <kevin60sanchez31@gmail.com>
2026-04-20 06:11:09 -0600

    commmit

--- saas-backend/prisma/seed.ts
+++ saas-backend/prisma/seed.ts
@@ -7,24 +7,38 @@ const hashProvider = new Argon2HashProvider()
 
 async function main() {
 
-    const negocio = await prisma.negocios.upsert({
+    const negocio = await prisma.negocio.upsert({
         where: { wa_id: "50230108703" },
         update: {},
         create: {
-            nombre_comercial: "Foxtin",
+            nombre: "ASYNCRONIX",
+            nombre_comercial: "ASYNCRONIX",
+            slug:  "asyncronix",
             wa_id: "50230108703",
             nit_rut: "388577959",
             slogan: "Todos juntos podemos",
         }
     });
 
-    const usuarioAdmin = await prisma.usuarios.upsert({
+
+    const modulos = await prisma.modulo.createMany({
+        data: [
+            {nombre: "USUARIOS"},
+            {nombre: "NEGOCIOS"},
+            {nombre: "PRODUCTOS"}
+        ]
+    })
+
+    const 
+
+    const usuarioAdmin = await prisma.user.upsert({
         where: { telefono: "30108703" },
         update: {},
         create: {
             nombre: "Kevin Eduardo",
-            email: "email"
+            email: "kevin@gmail.com",
             telefono: "30108703",
+            tipo:     "EMPLEADO",
             password_hash: await hashProvider.hash("12345678"),
             activo: true,
             verificado: false,
